d9aaeaf96714b3c856d0fe0bd77e47e0dca1ef97
Avishka Yohan <60248593+yohan98-dev@users.noreply.github.com>
2023-02-23 21:50:05 +0530

    mongo db connection and login and registration

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
 const express = require("express");
 const cors = require("cors");
+const { default: mongoose } = require("mongoose");
+const bcrypt = require("bcryptjs");
 const app = express();
+const User = require("./models/User");
+const jwt = require("jsonwebtoken");
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 
-app.post("/register", (req, res) => {
+const salt = bcrypt.genSaltSync(10);
+const secret = "asdfe45we45w345wegw345werjktjwertkj";
+
+mongoose.connect(
+  "mongodb+srv://yohan98:ngSwmqlNDBy7ZLw8@cluster0.7djhlwz.mongodb.net/?retryWrites=true&w=majority"
+);
+
+app.post("/register", async (req, res) => {
   const { username, password } = req.body;
-  res.json({ reqestData: { username, password } });
+  try {
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    res.status(400).json(e);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+
+  if (passOk) {
+    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie("token", token).json("ok");
+    });
+  } else {
+    res.status(400).json("Wrong credentials");
+  }
 });
 
 app.listen(4000);
+
+//ngSwmqlNDBy7ZLw8
+
+//mongodb+srv://yohan98:ngSwmqlNDBy7ZLw8@cluster0.7djhlwz.mongodb.net/?retryWrites=true&w=majority
